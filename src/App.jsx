a9b9d31dd9bbaa28302b5d869ed85fba04b6a7eb
Ali-imran-developer <ai8440018@gmail.com>
2024-09-27 09:05:58 +0500

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import './App.css'
-import { Outlet } from 'react-router-dom';
-import NavItems from './Components/NavItems';
-import Footer from './Components/Footer';
 
 function App() {
   return (
     <>
-      <NavItems />
-      <div className='min-vh-100'>
-        <Outlet />
+      <div>
+        This is Shop Cart Project
       </div>
-      <Footer />
     </>
   )
 }
